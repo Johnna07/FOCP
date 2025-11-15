b4c9045ffc9aa9fd76fa193d92d0bbd39347a6bb
Johnna07 <johnnamarythomas@gmail.com>
2025-11-15 23:35:53 +0530

    Refactor binary pattern program to pyramid format
    Updated the binary pattern program to print a binary pyramid pattern with proper formatting.

--- FOCP-1 ASSIGNMENT/q7.c
+++ FOCP-1 ASSIGNMENT/q7.c
@@ -1,21 +1,44 @@
-// binary pattern 
+
+//C program to print a binary pyramid pattern
 #include <stdio.h>
 
 int main() {
-    int n, i, j;
-    printf("Enter number of rows: ");
-    scanf("%d", &n);
-    for (i = 1; i <= n; i++) {
-        for (j = 1; j <= i; j++) {
-            printf("%d", (j+1)% 2); }
 
-        printf(" "); 
-        for (j = 1; j <= i; j++) {
-            printf("%d", (j+1) % 2);
+    int i, j;
+
+    for(i=1;i<=5;i++) {
+
+        for(j=1;j<=i;j++) {
+            if(j%2==0)
+                printf("1");
+            else
+                printf("0");
+        }
+
+        if(i!=5) {
+
+            for(j=1;j<=(5-i);j++) {
+                printf("  ");
+            }
+
+            for(j=1;j<=i;j++) {
+                if(j%2==0)
+                    printf("1");
+                else
+                    printf("0");
+            }
+        }
+
+        else {
+            for(j=1;j<=i;j++) {
+                if(j%2==0)
+                    printf("1");
+                else
+                    printf("0");
+            }
         }
 
         printf("\n");
     }
-
     return 0;
 }
